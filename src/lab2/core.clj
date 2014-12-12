77bf34c03d869543f2c36733b80a5ecfc3b59540
werzul <georgewerzul@gmail.com>
2014-12-12 19:19:09 +0300

    Initial commit + lein deps Enlive lib

---
+++ src/lab2/core.clj
@@ -0,0 +1,12 @@
+(ns lab2.core
+  (:gen-class)
+  (:require [net.cgrand.enlive-html :as html]))
+
+
+(require '[clojure.java.io :as io])
+(require '[clojure.string :as str])
+
+(defn -main
+	[&args]
+	(println empty)
+)
